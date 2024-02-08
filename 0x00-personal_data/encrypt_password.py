3f9d0d006200252637be175b545d6eab2eaabd8f
H. Sabah <sabahhasbi00@gmail.com>
2024-02-08 11:31:29 +0000

    Files added: task 5

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module encrypt_password"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
