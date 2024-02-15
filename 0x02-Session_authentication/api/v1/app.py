680ca02ad0abdb53eb2b97062b1f3633c3ec1de3
H. Sabah <sabahhasbi00@gmail.com>
2024-02-15 22:47:50 +0000

    Code fix: itty bitty typo

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,7 +30,7 @@ elif os.getenv("AUTH_TYPE") == "session_exp_auth":
 
     auth = SessionExpAuth()
 elif os.getenv("AUTH_TYPE") == "session_db_auth":
-    from api.v1.auth.session_db_auth import SessionDBpAuth
+    from api.v1.auth.session_db_auth import SessionDBAuth
 
     auth = SessionDBAuth()
 
