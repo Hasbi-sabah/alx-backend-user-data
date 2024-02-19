1fbbf8de3457e2edc66a72292f770806f68f0ce2
H. Sabah <sabahhasbi00@gmail.com>
2024-02-19 11:19:35 +0000

    Update: task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,11 +19,20 @@ class Auth:
         """Initialize a new instance of DB"""
         self._db = DB()
 
-    def register_user(self, email, password):
+    def register_user(self, email: str, password: str) -> User:
         """Register a new user with the provided email and password."""
         try:
-            user = self._db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
         except NoResultFound:
             user = self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError(f"User {email} already exists")
+        return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user login credentials."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode("utf-8"), user.hashed_password)
